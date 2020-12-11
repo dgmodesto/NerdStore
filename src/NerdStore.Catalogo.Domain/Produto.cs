268d98b4da1b9f9014408b8310853b986274a3b0
Douglas Gomes Modesto <d.modesto@guide.com.br>
2020-12-11 17:37:25 -0300

    project(nerd-store): criar projeto e criar model de produto e categoria

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,83 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        public Guid CategoriaId { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool  Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+
+        public Categoria Categoria { get; private set; }
+
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem, int quantidadeEstoque)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            CategoriaId = categoriaId;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+        }
+
+        public void Ativar() => Ativo = true;
+        public void Desativar() => Ativo = false;
+        public void AltetarCategoria(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaId = Categoria.Id;
+        }
+
+        public void AlterarDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+
+        public void DebitarEstoque(int quantidade)
+        {
+            if (quantidade < 0) quantidade *= -1;
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public void ReporEstoque(int quantidade)
+        {
+            QuantidadeEstoque += quantidade;
+        }
+
+        public bool PossuiEstoque(int quantidade)
+        {
+            return QuantidadeEstoque >= quantidade;
+        }
+
+        public void Validar()
+        {
+
+        }
+    }
+
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+        public int Codigo { get; private set; }
+
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+    }
+}
---
+++ src/NerdStore.Core/DomainObjects/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public interface IAggregateRoot
+    {
+    }
+}
